370c361ac9be837eeeb7a2e3756c553acae97407
youse <youseonho@gmail.com>
2026-03-21 12:12:23 +0900

    DB차트 로딩 시 20260211 업데이트 확인 및 안내 메시지 추가

--- utility/chart.py
+++ utility/chart.py
@@ -196,162 +196,171 @@ class Chart:
 
         if df is None or df.empty:
             self.windowQ.put((ui_num['차트'], '차트오류', '', '', '', ''))
+            return
+
+        if '당일매수금액' not in df.columns:
+            self.windowQ.put((
+                ui_num['시스템로그'],
+                '오류 알림 - 데이터베이스의 칼럼이 일치하지 않습니다.\n'
+                '프로그램을 종료하고 update_db_20260211.bat 파일을 실행하여 데이터베이스를 업데이트하십시오.'
+            ))
+            return
+
+        if cf1 is None:
+            arry = add_rolling_data(df, market, is_tick, [w_unit])
         else:
-            if cf1 is None:
-                arry = add_rolling_data(df, market, is_tick, [w_unit])
-            else:
-                arry = add_rolling_data(df, market, is_tick, [w_unit], cf1=cf1, cf2=cf2)
+            arry = add_rolling_data(df, market, is_tick, [w_unit], cf1=cf1, cf2=cf2)
 
-            buy_index  = []
-            sell_index = []
+        buy_index  = []
+        sell_index = []
 
+        arry = get_np().column_stack((arry, get_np().zeros((arry.shape[0], 2))))
+        if market in (2, 4):
             arry = get_np().column_stack((arry, get_np().zeros((arry.shape[0], 2))))
-            if market in (2, 4):
-                arry = get_np().column_stack((arry, get_np().zeros((arry.shape[0], 2))))
 
-            if detail is None:
-                con = sqlite3.connect(DB_TRADELIST)
-                if market in (3, 4):
-                    df = get_pd().read_sql(f"SELECT * FROM c_chegeollist WHERE 체결시간 LIKE '{searchdate}%' and 종목명 = '{code}'", con).set_index('index')
-                else:
-                    name = self.dict_name[code] if code in self.dict_name else code
-                    if market == 1:
-                        df = get_pd().read_sql(f"SELECT * FROM s_chegeollist WHERE 체결시간 LIKE '{searchdate}%' and 종목명 = '{name}'", con).set_index('index')
-                    else:
-                        df = get_pd().read_sql(f"SELECT * FROM f_chegeollist WHERE 체결시간 LIKE '{searchdate}%' and 종목명 = '{name}'", con).set_index('index')
-                con.close()
-
-                if len(df) > 0:
-                    for index in df.index:
-                        cgtime = int(df['체결시간'][index] if is_tick else str(df['체결시간'][index])[:12])
-                        if market in (1, 3):
-                            if df['주문구분'][index] == '매수':
-                                buy_index.append(get_cgtime(cgtime))
-                                arry[arry[:, 0] == cgtime, -2] = df['체결가'][index]
-
-                            elif df['주문구분'][index] == '매도':
-                                sell_index.append(get_cgtime(cgtime))
-                                arry[arry[:, 0] == cgtime, -1] = df['체결가'][index]
-                        else:
-                            if df['주문구분'][index] == 'BUY_LONG':
-                                buy_index.append(get_cgtime(cgtime))
-                                arry[arry[:, 0] == cgtime, -4] = df['체결가'][index]
-
-                            elif df['주문구분'][index] == 'SELL_LONG':
-                                sell_index.append(get_cgtime(cgtime))
-                                arry[arry[:, 0] == cgtime, -3] = df['체결가'][index]
-
-                            elif df['주문구분'][index] == 'SELL_SHORT':
-                                buy_index.append(get_cgtime(cgtime))
-                                arry[arry[:, 0] == cgtime, -2] = df['체결가'][index]
-
-                            elif df['주문구분'][index] == 'BUY_SHORT':
-                                sell_index.append(get_cgtime(cgtime))
-                                arry[arry[:, 0] == cgtime, -1] =  df['체결가'][index]
+        if detail is None:
+            con = sqlite3.connect(DB_TRADELIST)
+            if market in (3, 4):
+                df = get_pd().read_sql(f"SELECT * FROM c_chegeollist WHERE 체결시간 LIKE '{searchdate}%' and 종목명 = '{code}'", con).set_index('index')
             else:
-                매수시간, 매수가, 매도시간, 매도가 = detail
-                buy_index.append(매수시간)
-                sell_index.append(매도시간)
+                name = self.dict_name[code] if code in self.dict_name else code
+                if market == 1:
+                    df = get_pd().read_sql(f"SELECT * FROM s_chegeollist WHERE 체결시간 LIKE '{searchdate}%' and 종목명 = '{name}'", con).set_index('index')
+                else:
+                    df = get_pd().read_sql(f"SELECT * FROM f_chegeollist WHERE 체결시간 LIKE '{searchdate}%' and 종목명 = '{name}'", con).set_index('index')
+            con.close()
 
-                arry[arry[:, 0] == 매수시간, -2] = 매수가
-                arry[arry[:, 0] == 매도시간, -1] = 매도가
+            if len(df) > 0:
+                for index in df.index:
+                    cgtime = int(df['체결시간'][index] if is_tick else str(df['체결시간'][index])[:12])
+                    if market in (1, 3):
+                        if df['주문구분'][index] == '매수':
+                            buy_index.append(get_cgtime(cgtime))
+                            arry[arry[:, 0] == cgtime, -2] = df['체결가'][index]
 
-                if buytimes:
-                    buytimes = buytimes.split('^')
-                    buytimes = [x.split(';') for x in buytimes]
-                    for x in buytimes:
-                        추가매수시간, 추가매수가 = int(x[0]), int(x[1]) if market in (1, 2) else float(x[1])
-                        buy_index.append(추가매수시간)
-                        arry[arry[:, 0] == 추가매수시간, -2] = 추가매수가
+                        elif df['주문구분'][index] == '매도':
+                            sell_index.append(get_cgtime(cgtime))
+                            arry[arry[:, 0] == cgtime, -1] = df['체결가'][index]
+                    else:
+                        if df['주문구분'][index] == 'BUY_LONG':
+                            buy_index.append(get_cgtime(cgtime))
+                            arry[arry[:, 0] == cgtime, -4] = df['체결가'][index]
 
-            if not is_tick:
-                arry = get_np().column_stack((arry, get_np().zeros((arry.shape[0], 28))))
-                try:
-                    mc = arry[:, 1]
-                    mh = arry[:, self.factor_index['주식분봉고가' if market == 1 else '그외분봉고가']]
-                    ml = arry[:, self.factor_index['주식분봉저가' if market == 1 else '그외분봉저가']]
-                    mv = arry[:, self.factor_index['주식거래대금' if market == 1 else '그외거래대금']]
+                        elif df['주문구분'][index] == 'SELL_LONG':
+                            sell_index.append(get_cgtime(cgtime))
+                            arry[arry[:, 0] == cgtime, -3] = df['체결가'][index]
 
-                    AD = get_talib().AD(mh, ml, mc, mv)
-                    arry[:, -28] = AD
-                    if k[0] != 0:
-                        ADOSC = get_talib().ADOSC(mh, ml, mc, mv, fastperiod=k[0], slowperiod=k[1])
-                        arry[:, -27] = ADOSC
-                    if k[2] != 0:
-                        ADXR = get_talib().ADXR(mh, ml, mc, timeperiod=k[2])
-                        arry[:, -26] = ADXR
-                    if k[3] != 0:
-                        APO = get_talib().APO(mc, fastperiod=k[3], slowperiod=k[4], matype=k[5])
-                        arry[:, -25] = APO
-                    if k[6] != 0:
-                        AROOND, AROONU = get_talib().AROON(mh, ml, timeperiod=k[6])
-                        arry[:, -24] = AROOND
-                        arry[:, -23] = AROONU
-                    if k[7] != 0:
-                        ATR = get_talib().ATR(mh, ml, mc, timeperiod=k[7])
-                        arry[:, -22] = ATR
-                    if k[8] != 0:
-                        BBU, BBM, BBL = get_talib().BBANDS(mc, timeperiod=k[8], nbdevup=k[9], nbdevdn=k[10], matype=k[11])
-                        arry[:, -21] = BBU
-                        arry[:, -20] = BBM
-                        arry[:, -19] = BBL
-                    if k[12] != 0:
-                        CCI = get_talib().CCI(mh, ml, mc, timeperiod=k[12])
-                        arry[:, -18] = CCI
-                    if k[13] != 0:
-                        DIM = get_talib().MINUS_DI(mh, ml, mc, timeperiod=k[13])
-                        DIP = get_talib().PLUS_DI(mh, ml, mc, timeperiod=k[13])
-                        arry[:, -17] = DIM
-                        arry[:, -16] = DIP
-                    if k[14] != 0:
-                        MACD, MACDS, MACDH = get_talib().MACD(mc, fastperiod=k[14], slowperiod=k[15], signalperiod=k[16])
-                        arry[:, -15] = MACD
-                        arry[:, -14] = MACDS
-                        arry[:, -13] = MACDH
-                    if k[17] != 0:
-                        MFI = get_talib().MFI(mh, ml, mc, mv, timeperiod=k[17])
-                        arry[:, -12] = MFI
-                    if k[18] != 0:
-                        MOM = get_talib().MOM(mc, timeperiod=k[18])
-                        arry[:, -11] = MOM
-                    OBV = get_talib().OBV(mc, mv)
-                    arry[:, -10] = OBV
-                    if k[19] != 0:
-                        PPO = get_talib().PPO(mc, fastperiod=k[19], slowperiod=k[20], matype=k[21])
-                        arry[:,  -9] = PPO
-                    if k[22] != 0:
-                        ROC = get_talib().ROC(mc, timeperiod=k[22])
-                        arry[:,  -8] = ROC
-                    if k[23] != 0:
-                        RSI = get_talib().RSI(mc, timeperiod=k[23])
-                        arry[:,  -7] = RSI
-                    if k[24] != 0:
-                        SAR = get_talib().SAR(mh, ml, acceleration=k[24], maximum=k[25])
-                        arry[:,  -6] = SAR
-                    if k[26] != 0:
-                        STOCHSK, STOCHSD = get_talib().STOCH(mh, ml, mc, fastk_period=k[26], slowk_period=k[27], slowk_matype=k[28], slowd_period=k[29], slowd_matype=k[30])
-                        arry[:,  -5] = STOCHSK
-                        arry[:,  -4] = STOCHSD
-                    if k[31] != 0:
-                        STOCHFK, STOCHFD = get_talib().STOCHF(mh, ml, mc, fastk_period=k[31], fastd_period=k[32], fastd_matype=k[33])
-                        arry[:,  -3] = STOCHFK
-                        arry[:,  -2] = STOCHFD
-                    if k[34] != 0:
-                        WILLR = get_talib().WILLR(mh, ml, mc, timeperiod=k[34])
-                        arry[:,  -1] = WILLR
-                    arry = get_np().nan_to_num(arry)
-                except:
-                    arry = None
-                    self.windowQ.put((ui_num['시스템로그'], f'{format_exc()}오류 알림 - 보조지표의 설정값이 잘못되었습니다.'))
+                        elif df['주문구분'][index] == 'SELL_SHORT':
+                            buy_index.append(get_cgtime(cgtime))
+                            arry[arry[:, 0] == cgtime, -2] = df['체결가'][index]
 
-            fm_list, dict_fm, fm_tcnt = get_formula_data(True, arry.shape[1])
-            if fm_tcnt > 0:
-                arry = get_np().column_stack((arry, get_np().zeros((arry.shape[0], fm_tcnt))))
-                fm = FormulaManager(deepcopy(fm_list))
-                fm.update_all_data(code, arry, market, is_tick, w_unit)
+                        elif df['주문구분'][index] == 'BUY_SHORT':
+                            sell_index.append(get_cgtime(cgtime))
+                            arry[arry[:, 0] == cgtime, -1] =  df['체결가'][index]
+        else:
+            매수시간, 매수가, 매도시간, 매도가 = detail
+            buy_index.append(매수시간)
+            sell_index.append(매도시간)
 
-            if arry is not None:
-                if is_tick: xticks = [dt_ymdhms(str(int(x))).timestamp() for x in arry[:, 0]]
-                else:       xticks = [dt_ymdhms(f'{int(x)}00').timestamp() for x in arry[:, 0]]
-                gubun = 'C' if coin else 'S' if '키움증권' in self.dict_set['증권사'] else 'F'
-                self.windowQ.put((ui_num['차트'], gubun, xticks, arry, buy_index, sell_index, fm_list, dict_fm, fm_tcnt))
+            arry[arry[:, 0] == 매수시간, -2] = 매수가
+            arry[arry[:, 0] == 매도시간, -1] = 매도가
+
+            if buytimes:
+                buytimes = buytimes.split('^')
+                buytimes = [x.split(';') for x in buytimes]
+                for x in buytimes:
+                    추가매수시간, 추가매수가 = int(x[0]), int(x[1]) if market in (1, 2) else float(x[1])
+                    buy_index.append(추가매수시간)
+                    arry[arry[:, 0] == 추가매수시간, -2] = 추가매수가
+
+        if not is_tick:
+            arry = get_np().column_stack((arry, get_np().zeros((arry.shape[0], 28))))
+            try:
+                mc = arry[:, 1]
+                mh = arry[:, self.factor_index['주식분봉고가' if market == 1 else '그외분봉고가']]
+                ml = arry[:, self.factor_index['주식분봉저가' if market == 1 else '그외분봉저가']]
+                mv = arry[:, self.factor_index['주식거래대금' if market == 1 else '그외거래대금']]
+
+                AD = get_talib().AD(mh, ml, mc, mv)
+                arry[:, -28] = AD
+                if k[0] != 0:
+                    ADOSC = get_talib().ADOSC(mh, ml, mc, mv, fastperiod=k[0], slowperiod=k[1])
+                    arry[:, -27] = ADOSC
+                if k[2] != 0:
+                    ADXR = get_talib().ADXR(mh, ml, mc, timeperiod=k[2])
+                    arry[:, -26] = ADXR
+                if k[3] != 0:
+                    APO = get_talib().APO(mc, fastperiod=k[3], slowperiod=k[4], matype=k[5])
+                    arry[:, -25] = APO
+                if k[6] != 0:
+                    AROOND, AROONU = get_talib().AROON(mh, ml, timeperiod=k[6])
+                    arry[:, -24] = AROOND
+                    arry[:, -23] = AROONU
+                if k[7] != 0:
+                    ATR = get_talib().ATR(mh, ml, mc, timeperiod=k[7])
+                    arry[:, -22] = ATR
+                if k[8] != 0:
+                    BBU, BBM, BBL = get_talib().BBANDS(mc, timeperiod=k[8], nbdevup=k[9], nbdevdn=k[10], matype=k[11])
+                    arry[:, -21] = BBU
+                    arry[:, -20] = BBM
+                    arry[:, -19] = BBL
+                if k[12] != 0:
+                    CCI = get_talib().CCI(mh, ml, mc, timeperiod=k[12])
+                    arry[:, -18] = CCI
+                if k[13] != 0:
+                    DIM = get_talib().MINUS_DI(mh, ml, mc, timeperiod=k[13])
+                    DIP = get_talib().PLUS_DI(mh, ml, mc, timeperiod=k[13])
+                    arry[:, -17] = DIM
+                    arry[:, -16] = DIP
+                if k[14] != 0:
+                    MACD, MACDS, MACDH = get_talib().MACD(mc, fastperiod=k[14], slowperiod=k[15], signalperiod=k[16])
+                    arry[:, -15] = MACD
+                    arry[:, -14] = MACDS
+                    arry[:, -13] = MACDH
+                if k[17] != 0:
+                    MFI = get_talib().MFI(mh, ml, mc, mv, timeperiod=k[17])
+                    arry[:, -12] = MFI
+                if k[18] != 0:
+                    MOM = get_talib().MOM(mc, timeperiod=k[18])
+                    arry[:, -11] = MOM
+                OBV = get_talib().OBV(mc, mv)
+                arry[:, -10] = OBV
+                if k[19] != 0:
+                    PPO = get_talib().PPO(mc, fastperiod=k[19], slowperiod=k[20], matype=k[21])
+                    arry[:,  -9] = PPO
+                if k[22] != 0:
+                    ROC = get_talib().ROC(mc, timeperiod=k[22])
+                    arry[:,  -8] = ROC
+                if k[23] != 0:
+                    RSI = get_talib().RSI(mc, timeperiod=k[23])
+                    arry[:,  -7] = RSI
+                if k[24] != 0:
+                    SAR = get_talib().SAR(mh, ml, acceleration=k[24], maximum=k[25])
+                    arry[:,  -6] = SAR
+                if k[26] != 0:
+                    STOCHSK, STOCHSD = get_talib().STOCH(mh, ml, mc, fastk_period=k[26], slowk_period=k[27], slowk_matype=k[28], slowd_period=k[29], slowd_matype=k[30])
+                    arry[:,  -5] = STOCHSK
+                    arry[:,  -4] = STOCHSD
+                if k[31] != 0:
+                    STOCHFK, STOCHFD = get_talib().STOCHF(mh, ml, mc, fastk_period=k[31], fastd_period=k[32], fastd_matype=k[33])
+                    arry[:,  -3] = STOCHFK
+                    arry[:,  -2] = STOCHFD
+                if k[34] != 0:
+                    WILLR = get_talib().WILLR(mh, ml, mc, timeperiod=k[34])
+                    arry[:,  -1] = WILLR
+                arry = get_np().nan_to_num(arry)
+            except:
+                arry = None
+                self.windowQ.put((ui_num['시스템로그'], f'{format_exc()}오류 알림 - 보조지표의 설정값이 잘못되었습니다.'))
+
+        fm_list, dict_fm, fm_tcnt = get_formula_data(True, arry.shape[1])
+        if fm_tcnt > 0:
+            arry = get_np().column_stack((arry, get_np().zeros((arry.shape[0], fm_tcnt))))
+            fm = FormulaManager(deepcopy(fm_list))
+            fm.update_all_data(code, arry, market, is_tick, w_unit)
+
+        if arry is not None:
+            if is_tick: xticks = [dt_ymdhms(str(int(x))).timestamp() for x in arry[:, 0]]
+            else:       xticks = [dt_ymdhms(f'{int(x)}00').timestamp() for x in arry[:, 0]]
+            gubun = 'C' if coin else 'S' if '키움증권' in self.dict_set['증권사'] else 'F'
+            self.windowQ.put((ui_num['차트'], gubun, xticks, arry, buy_index, sell_index, fm_list, dict_fm, fm_tcnt))
