73e4cf29267de83aa872f1780a2d27831811c6f4
youse <youseonho@gmail.com>
2026-03-22 12:21:14 +0900

    UI 임포트 후크 순서 정리

--- ui/ui_import_hook.py
+++ ui/ui_import_hook.py
@@ -9,27 +9,28 @@ class ImportProgressHook:
         self.original_import = None
         self.modules = [
             'ui.ui_mainwindow', 'ui.set_style', 'utility.hoga', 'utility.query', 'utility.chart', 'utility.sound',
-            'utility.timesync', 'utility.setting_base', 'utility.webcrawling', 'utility.telegram_bot',
-            'utility.setting_user', 'utility.database_check', 'utility.database_read_only', 'ui.set_icon',
-            'ui.set_table', 'ui.set_log_tap', 'ui.set_widget', 'ui.set_setup_tap', 'ui.set_order_tap',
+            'utility.timesync', 'utility.setting_base', 'utility.webcrawling', 'utility.static', 'utility.telegram_bot',
+            'utility.setting_user', 'utility.webcrawling_homtab', 'utility.database_read_only', 'ui.set_icon',
+            'ui.set_table', 'ui.set_log_tap', 'ui.set_home_tap', 'ui.set_widget', 'ui.set_setup_tap', 'ui.set_order_tap',
             'ui.set_main_menu', 'ui.set_dialog_etc', 'ui.set_dialog_back', 'ui.set_stg_coin_tap', 'ui.set_dialog_chart',
-            'ui.set_stg_stock_tap', 'ui.set_dialog_formula', 'ui.set_dialog_strategy', 'ui.ui_draw_chart_db',
-            'ui.ui_draw_treemap', 'ui.ui_process_kill', 'ui.ui_load_database', 'ui.ui_extend_window',
-            'ui.ui_draw_chart_real', 'ui.ui_update_textedit', 'ui.ui_process_starter', 'utility.webcrawling_homtab'
+            'ui.set_stg_stock_tap', 'ui.set_dialog_formula', 'ui.set_dialog_strategy', 'ui.ui_draw_treemap',
+            'ui.ui_draw_chart_db', 'ui.ui_process_kill', 'ui.ui_load_database', 'ui.ui_extend_window',
+            'ui.ui_draw_home_chart', 'ui.ui_draw_chart_real', 'ui.ui_update_textedit', 'ui.ui_process_starter',
             'ui.ui_draw_jisuchart', 'ui.ui_update_tablewidget', 'ui.ui_chart_count_change', 'ui.ui_etc',
             'ui.ui_show_dialog', 'ui.ui_vars_change', 'ui.ui_event_filter', 'ui.ui_return_press', 'ui.ui_text_changed',
-            'ui.ui_cell_clicked', 'ui.ui_activated_etc', 'ui.ui_process_alive', 'ui.ui_button_clicked_dialog_formula',
-            'ui.ui_activated_back', 'ui.ui_checkbox_changed', 'ui.ui_key_press_event', 'ui.ui_backtest_engine',
-            'ui.ui_update_progressbar', 'ui.ui_button_clicked_etc', 'ui.ui_activated_coin_stg',
-            'ui.ui_button_clicked_zoom', 'ui.ui_activated_stock_stg', 'ui.ui_button_clicked_order',
-            'ui.ui_button_clicked_chart', 'ui.ui_button_clicked_shortcut', 'ui.ui_button_clicked_strategy',
-            'ui.ui_button_clicked_settings', 'ui.ui_button_clicked_editer_coin', 'ui.ui_button_clicked_editer_stock',
-            'ui.ui_button_clicked_editer_ga_coin', 'ui.ui_button_clicked_editer_backlog',
-            'ui.ui_button_clicked_dialog_database', 'ui.ui_button_clicked_editer_ga_stock',
-            'ui.ui_button_clicked_editer_opti_coin', 'ui.ui_button_clicked_dialog_backengine',
-            'ui.ui_button_clicked_editer_opti_stock', 'ui.ui_button_clicked_editer_stg_buy_coin',
-            'ui.ui_button_clicked_editer_stg_sell_coin', 'ui.ui_button_clicked_editer_stg_buy_stock',
-            'ui.ui_button_clicked_editer_stg_sell_stock', 'ui.ui_button_clicked_dialog_elapsed_tick_number'
+            'ui.ui_cell_clicked', 'ui.ui_activated_etc', 'ui.ui_process_alive', 'ui.ui_activated_back',
+            'ui.ui_key_press_event', 'ui.ui_backtest_engine', 'ui.ui_checkbox_changed', 'ui.ui_update_progressbar',
+            'ui.ui_button_clicked_etc', 'ui.ui_activated_coin_stg', 'ui.ui_button_clicked_zoom',
+            'ui.ui_activated_stock_stg', 'ui.ui_button_clicked_order', 'ui.ui_button_clicked_chart',
+            'ui.ui_button_clicked_shortcut', 'ui.ui_button_clicked_strategy', 'ui.ui_button_clicked_settings',
+            'ui.ui_button_clicked_editer_coin', 'ui.ui_button_clicked_editer_stock',
+            'ui.ui_button_clicked_dialog_formula', 'ui.ui_button_clicked_editer_ga_coin',
+            'ui.ui_button_clicked_editer_backlog', 'ui.ui_button_clicked_dialog_database',
+            'ui.ui_button_clicked_editer_ga_stock', 'ui.ui_button_clicked_editer_opti_coin',
+            'ui.ui_button_clicked_dialog_backengine', 'ui.ui_button_clicked_editer_opti_stock',
+            'ui.ui_button_clicked_editer_stg_buy_coin', 'ui.ui_button_clicked_editer_stg_sell_coin',
+            'ui.ui_button_clicked_editer_stg_buy_stock', 'ui.ui_button_clicked_editer_stg_sell_stock',
+            'ui.ui_button_clicked_dialog_elapsed_tick_number'
         ]
         self.total_modules = len(self.modules)
         self.current_index = 0
@@ -37,7 +38,7 @@ class ImportProgressHook:
     def custom_import(self, name, *args, **kwargs):
         if name in self.modules:
             self.current_index += 1
-            progress = self.progress + (self.current_index / self.total_modules) * 76
+            progress = self.progress + (self.current_index / self.total_modules) * 79
             self.splash.show_progress(f"{name}...", int(progress))
         return self.original_import(name, *args, **kwargs)
 
