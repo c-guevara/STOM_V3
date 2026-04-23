a64a754c0b295ce7fe7aaa4bdd7a574dfb43b54d
youse <youseonho@gmail.com>
2026-04-24 08:49:58 +0900

    불필요한 쌍따옴표, 외따옴표로 변경

--- utility/db_control/database_check.py
+++ utility/db_control/database_check.py
@@ -14,14 +14,14 @@ MAIN_DATA = [
     [0, '국내주식01', 1, 1, 1, 1, '', 1, 1, '0;5;1^5;10;2^10;20;3^20;30;4^30;100;5', 'ISOLATED', 'false']
 ]
 
-ACCOUNT_CLOUMNS = ["index", "access_key", "secret_key"]
+ACCOUNT_CLOUMNS = ['index', 'access_key', 'secret_key']
 ACCOUNT_DATA = [
     [1, '', ''], [2, '', ''], [3, '', ''], [4, '', ''], [5, '', ''], [6, '', ''],
     [7, '', ''], [8, '', ''], [9, '', ''], [10, '', ''], [11, '', ''], [12, '', ''],
     [13, '', ''], [14, '', ''], [15, '', ''], [16, '', ''], [17, '', ''], [18, '', '']
 ]
 
-TELE_CLOUMNS = ["index", "bot_token", "chatingid"]
+TELE_CLOUMNS = ['index', 'bot_token', 'chatingid']
 TELE_DATA = [
     [1, '', ''], [2, '', ''], [3, '', ''], [4, '', ''], [5, '', ''], [6, '', ''],
     [7, '', ''], [8, '', ''], [9, '', ''], [10, '', ''], [11, '', ''], [12, '', ''],
@@ -29,9 +29,8 @@ TELE_DATA = [
 ]
 
 STG_CLOUMNS = [
-    "index", "매수전략", "매도전략", "평균값계산틱수", "최대매수종목수",
-    "전략종료시간", "잔고청산", "프로세스종료", "컴퓨터종료", "투자금고정", "투자금", "손실중지",
-    "손실중지수익률", "수익중지", "수익중지수익률", "블랙리스트"
+    'index', '매수전략', '매도전략', '평균값계산틱수', '최대매수종목수', '전략종료시간', '잔고청산', '프로세스종료', '컴퓨터종료',
+    '투자금고정', '투자금', '손실중지', '손실중지수익률', '수익중지', '수익중지수익률', '블랙리스트'
 ]
 STG_DATA = [
     [1, '', '', 30, 10, 93000, 1, 1, 0, 1, 20.0, 0, 2.0, 0, 2.0, ''],
@@ -55,11 +54,10 @@ STG_DATA = [
 ]
 
 BACT_CLOUMNS = [
-    "index", "블랙리스트추가", "백테주문관리적용", "백테매수시간기준", "백테일괄로딩", "그래프저장하지않기", "그래프띄우지않기",
-    "디비자동관리", "교차검증가중치", "기준값최소상승률", "백테스케쥴실행", "백테스케쥴요일", "백테스케쥴시간",
-    "백테스케쥴명", "백테날짜고정", "백테날짜", "최적화기준값제한", "백테엔진분류방법", "옵튜나샘플러", "옵튜나고정변수",
-    "옵튜나실행횟수", "옵튜나자동스탭", "범위자동관리", "보조지표설정", "백테스트로그기록안함", "시장미시구조분석", '리스크분석',
-    "자동학습", "패턴분석", "가격대분석"
+    'index', '블랙리스트추가', '백테주문관리적용', '백테매수시간기준', '백테일괄로딩', '그래프저장하지않기', '그래프띄우지않기', '디비자동관리',
+    '교차검증가중치', '기준값최소상승률', '백테스케쥴실행', '백테스케쥴요일', '백테스케쥴시간', '백테스케쥴명', '백테날짜고정', '백테날짜',
+    '최적화기준값제한', '백테엔진분류방법', '옵튜나샘플러', '옵튜나고정변수', '옵튜나실행횟수', '옵튜나자동스탭', '범위자동관리', '보조지표설정',
+    '백테스트로그기록안함', '시장미시구조분석', '리스크분석', '자동학습', '패턴분석', '가격대분석'
 ]
 
 BACT_DATA_BASE = [
@@ -72,8 +70,8 @@ BACT_DATA_BASE = [
 BACT_DATA = [[i+1] + BACT_DATA_BASE for i in range(18)]
 
 ETC_CLOUMNS = [
-    "index", "테마", "저해상도", "휴무프로세스종료", "휴무컴퓨터종료", "창위치기억", "창위치", "스톰라이브",
-    "프로그램종료", "웹대시보드", "웹대시보드포트번호", "팩터선택", "시가총액상위제외목록", "시리얼키"
+    'index', '테마', '저해상도', '휴무프로세스종료', '휴무컴퓨터종료', '창위치기억', '창위치', '스톰라이브', '프로그램종료', '웹대시보드',
+    '웹대시보드포트번호', '팩터선택', '시가총액상위제외목록', '시리얼키'
 ]
 EXCLUSION_LIST = [
     '000150', '000270', '000660', '000720', '000810', '003230', '003550', '003670', '005380', '005490',
@@ -91,12 +89,11 @@ ETC_DATA_BASE = [
 ETC_DATA = [[i+1] + ETC_DATA_BASE for i in range(18)]
 
 BORDER_CLOUMNS = [
-    'index', '매수주문유형', '매수분할횟수', '매수분할방법', '매수분할시그널', '매수분할하방', '매수분할상방',
-    '매수분할하방수익률', '매수분할상방수익률', '매수분할고정수익률', '매수지정가기준가격', '매수지정가호가번호',
-    '매수시장가잔량범위', '매수취소관심이탈', '매수취소매도시그널', '매수취소시간', '매수취소시간초', '매수금지블랙리스트',
-    '매수금지손절횟수', '매수금지손절횟수값', '매수금지거래횟수',
-    '매수금지거래횟수값', '매수금지시간', '매수금지시작시간', '매수금지종료시간', '매수금지간격', '매수금지간격초',
-    '매수금지손절간격', '매수금지손절간격초', '매수정정횟수', '매수정정호가차이', '매수정정호가', '비중조절'
+    'index', '매수주문유형', '매수분할횟수', '매수분할방법', '매수분할시그널', '매수분할하방', '매수분할상방', '매수분할하방수익률',
+    '매수분할상방수익률', '매수분할고정수익률', '매수지정가기준가격', '매수지정가호가번호', '매수시장가잔량범위', '매수취소관심이탈',
+    '매수취소매도시그널', '매수취소시간', '매수취소시간초', '매수금지블랙리스트', '매수금지손절횟수', '매수금지손절횟수값', '매수금지거래횟수',
+    '매수금지거래횟수값', '매수금지시간', '매수금지시작시간', '매수금지종료시간', '매수금지간격', '매수금지간격초', '매수금지손절간격',
+    '매수금지손절간격초', '매수정정횟수', '매수정정호가차이', '매수정정호가', '비중조절'
 ]
 BORDER_DATA_BASE = [
     '시장가', 1, 2, 1, 0, 1, 0.5, 0.5, 1, '매수1호가', 0, 3, 0, 0, 0, 30, 0, 0, 2, 0, 2, 0,
@@ -105,13 +102,11 @@ BORDER_DATA_BASE = [
 BORDER_DATA = [[i+1] + BORDER_DATA_BASE for i in range(18)]
 
 SORDER_CLOUMNS = [
-    'index', '매도주문유형', '매도분할횟수', '매도분할방법', '매도분할시그널', '매도분할하방', '매도분할상방',
-    '매도분할하방수익률', '매도분할상방수익률', '매도지정가기준가격', '매도지정가호가번호', '매도시장가잔량범위',
-    '매도취소관심진입', '매도취소매수시그널', '매도취소시간', '매도취소시간초', '매도금지매수횟수',
-    '매도금지매수횟수값', '매도금지시간', '매도금지시작시간',
-    '매도금지종료시간', '매도금지간격', '매도금지간격초', '매도정정횟수', '매도정정호가차이', '매도정정호가',
-    '매도익절수익률청산', '매도익절수익률', '매도익절수익금청산', '매도익절수익금',
-    '매도손절수익률청산', '매도손절수익률', '매도손절수익금청산', '매도손절수익금'
+    'index', '매도주문유형', '매도분할횟수', '매도분할방법', '매도분할시그널', '매도분할하방', '매도분할상방', '매도분할하방수익률',
+    '매도분할상방수익률', '매도지정가기준가격', '매도지정가호가번호', '매도시장가잔량범위', '매도취소관심진입', '매도취소매수시그널',
+    '매도취소시간', '매도취소시간초', '매도금지매수횟수', '매도금지매수횟수값', '매도금지시간', '매도금지시작시간', '매도금지종료시간',
+    '매도금지간격', '매도금지간격초', '매도정정횟수', '매도정정호가차이', '매도정정호가', '매도익절수익률청산', '매도익절수익률',
+    '매도익절수익금청산', '매도익절수익금', '매도손절수익률청산', '매도손절수익률', '매도손절수익금청산', '매도손절수익금'
 ]
 SORDER_DATA_BASE = [
     '시장가', 1, 1, 1, 0, 1, 0.5, 2.0, '매도1호가', 0, 5, 0, 0, 0, 30, 0, 2, 0,
@@ -357,14 +352,14 @@ def database_check():
                     elif table_name == '_totaltradelist':
                         cur.execute(f'''
                             CREATE TABLE {table_name} (
-                                "index" TEXT,
-                                "거래횟수" INTEGER,
-                                "총매수금액" INTEGER,
-                                "총매도금액" INTEGER,
-                                "총수익금액" INTEGER,
-                                "총손실금액" INTEGER,
-                                "수익률" REAL,
-                                "수익금합계" INTEGER,
+                                'index' TEXT,
+                                '거래횟수' INTEGER,
+                                '총매수금액' INTEGER,
+                                '총매도금액' INTEGER,
+                                '총수익금액' INTEGER,
+                                '총손실금액' INTEGER,
+                                '수익률' REAL,
+                                '수익금합계' INTEGER,
                                 PRIMARY KEY ('index')
                             )
                         ''')
@@ -372,87 +367,87 @@ def database_check():
                         if market == 'coin_future':
                             cur.execute(f'''
                                 CREATE TABLE {table_name} (
-                                    "index" TEXT,
-                                    "종목명" TEXT,
-                                    "포지션" TEXT,
-                                    "매수가" REAL,
-                                    "현재가" REAL,
-                                    "수익률" REAL,
-                                    "평가손익" INTEGER,
-                                    "매입금액" INTEGER,
-                                    "평가금액" INTEGER,
-                                    "보유수량" REAL,
-                                    "분할매수횟수" INTEGER,
-                                    "분할매도횟수" INTEGER,
-                                    "매수시간" TEXT,
-                                    "레버리지" INTEGER,
+                                    'index' TEXT,
+                                    '종목명' TEXT,
+                                    '포지션' TEXT,
+                                    '매수가' REAL,
+                                    '현재가' REAL,
+                                    '수익률' REAL,
+                                    '평가손익' INTEGER,
+                                    '매입금액' INTEGER,
+                                    '평가금액' INTEGER,
+                                    '보유수량' REAL,
+                                    '분할매수횟수' INTEGER,
+                                    '분할매도횟수' INTEGER,
+                                    '매수시간' TEXT,
+                                    '레버리지' INTEGER,
                                     PRIMARY KEY ('index')
                                 )
                             ''')
                         elif 'future' in market:
                             cur.execute(f'''
                                 CREATE TABLE {table_name} (
-                                    "index" TEXT,
-                                    "종목명" TEXT,
-                                    "포지션" TEXT,
-                                    "매수가" REAL,
-                                    "현재가" REAL,
-                                    "수익률" REAL,
-                                    "평가손익" INTEGER,
-                                    "매입금액" INTEGER,
-                                    "평가금액" INTEGER,
-                                    "보유수량" REAL,
-                                    "분할매수횟수" INTEGER,
-                                    "분할매도횟수" INTEGER,
-                                    "매수시간" TEXT,
+                                    'index' TEXT,
+                                    '종목명' TEXT,
+                                    '포지션' TEXT,
+                                    '매수가' REAL,
+                                    '현재가' REAL,
+                                    '수익률' REAL,
+                                    '평가손익' INTEGER,
+                                    '매입금액' INTEGER,
+                                    '평가금액' INTEGER,
+                                    '보유수량' REAL,
+                                    '분할매수횟수' INTEGER,
+                                    '분할매도횟수' INTEGER,
+                                    '매수시간' TEXT,
                                     PRIMARY KEY ('index')
                                 )
                             ''')
                         else:
                             cur.execute(f'''
                                 CREATE TABLE {table_name} (
-                                    "index" TEXT,
-                                    "종목명" TEXT,
-                                    "매수가" REAL,
-                                    "현재가" REAL,
-                                    "수익률" REAL,
-                                    "평가손익" INTEGER,
-                                    "매입금액" INTEGER,
-                                    "평가금액" INTEGER,
-                                    "보유수량" REAL,
-                                    "분할매수횟수" INTEGER,
-                                    "분할매도횟수" INTEGER,
-                                    "매수시간" TEXT,
+                                    'index' TEXT,
+                                    '종목명' TEXT,
+                                    '매수가' REAL,
+                                    '현재가' REAL,
+                                    '수익률' REAL,
+                                    '평가손익' INTEGER,
+                                    '매입금액' INTEGER,
+                                    '평가금액' INTEGER,
+                                    '보유수량' REAL,
+                                    '분할매수횟수' INTEGER,
+                                    '분할매도횟수' INTEGER,
+                                    '매수시간' TEXT,
                                     PRIMARY KEY ('index')
                                 )
                             ''')
                     else:
-                        if 'future' not in market:
+                        if 'future' in market:
                             cur.execute(f'''
                                 CREATE TABLE {table_name} (
-                                    "index" TEXT,
-                                    "종목명" TEXT,
-                                    "포지션" TEXT,
-                                    "매수금액" INTEGER,
-                                    "매도금액" INTEGER,
-                                    "주문수량" REAL,
-                                    "수익률" REAL,
-                                    "수익금" INTEGER,
-                                    "체결시간" TEXT,
+                                    'index' TEXT,
+                                    '종목명' TEXT,
+                                    '포지션' TEXT,
+                                    '매수금액' INTEGER,
+                                    '매도금액' INTEGER,
+                                    '주문수량' REAL,
+                                    '수익률' REAL,
+                                    '수익금' INTEGER,
+                                    '체결시간' TEXT,
                                     PRIMARY KEY ('index')
                                 )
                             ''')
                         else:
                             cur.execute(f'''
                                 CREATE TABLE {table_name} (
-                                    "index" TEXT,
-                                    "종목명" TEXT,
-                                    "매수금액" INTEGER,
-                                    "매도금액" INTEGER,
-                                    "주문수량" REAL,
-                                    "수익률" REAL,
-                                    "수익금" INTEGER,
-                                    "체결시간" TEXT,
+                                    'index' TEXT,
+                                    '종목명' TEXT,
+                                    '매수금액' INTEGER,
+                                    '매도금액' INTEGER,
+                                    '주문수량' REAL,
+                                    '수익률' REAL,
+                                    '수익금' INTEGER,
+                                    '체결시간' TEXT,
                                     PRIMARY KEY ('index')
                                 )
                             ''')
@@ -473,7 +468,7 @@ def database_check():
                 if 'moneytop' in table_list:
                     table_list.remove('moneytop')
                 if table_list:
-                    df = pd.read_sql(f'SELECT * FROM "{table_list[0]}"', con)
+                    df = pd.read_sql(f"SELECT * FROM '{table_list[0]}'", con)
                     idx_ask1 = list(df.columns).index('매도호가1')
                     idx_ask2 = list(df.columns).index('매도호가2')
                     if '전일비' in df.columns or '당일매수금액' not in df.columns or idx_ask2 != idx_ask1 + 1:
@@ -492,7 +487,7 @@ def database_check():
                     if table in table_list:
                         table_list.remove(table)
                 if table_list:
-                    df = pd.read_sql(f'SELECT * FROM "{table_list[0]}"', con)
+                    df = pd.read_sql(f"SELECT * FROM '{table_list[0]}'", con)
                     if '당일매수금액' not in df.columns:
                         con.close()
                         return False, f'백테DB의 칼럼이 일치하지 않습니다.\n업데이트 된 일자DB로 백테DB를 새로 생성하십시오.'
